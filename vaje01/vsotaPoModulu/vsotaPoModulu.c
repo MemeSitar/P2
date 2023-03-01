504c467e7ae87926e45adb07a2f725aaab983404
MemeSitar <jurij.sitar@mjau.si>
2023-03-01 09:53:25 +0100

    dela!

--- vaje01/vsotaPoModulu/vsotaPoModulu.c
+++ vaje01/vsotaPoModulu/vsotaPoModulu.c
@@ -3,9 +3,11 @@
 int main() {
     int a, b, c;
     a = getchar() - '0'; // pretvori v dejansko stevilo
+    getchar();
     b = getchar() - '0'; 
     c = (a + b) % 10;
     putchar(c + '0');
+    putchar('\n');
     
     return 0;
 }
