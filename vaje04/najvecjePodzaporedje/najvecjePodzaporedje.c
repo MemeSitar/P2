65c34256578ffec424237fad7d1ed66a0e9e30a5
MemeSitar <jurij.sitar@mjau.si>
2023-03-22 10:44:12 +0100

    optimalna rešitev.

--- vaje04/najvecjePodzaporedje/najvecjePodzaporedje.c
+++ vaje04/najvecjePodzaporedje/najvecjePodzaporedje.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <limits.h>
 
 int tabela[1000000];
 
 int main() {
     int n;
     int vsota = 0;
-    bool soPozitivna = false;
+    //bool soPozitivna = false;
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++){
+    /*for (int i = 0; i < n; i++){
         scanf("%d", &tabela[i]);
         if (!soPozitivna){
             if (tabela[i] > 0){
                 soPozitivna = true;
             }
         }
-    }
-    
-    int najVsota = tabela[0];
+    }*/
+
+    int najVsota = INT_MIN;
+    int clen = 0;
     for (int i = 0; i < n; i++){
-        vsota = 0;
-        if (soPozitivna && tabela[i] < 0){
+        scanf("%d", &clen);
+        vsota += clen;
+        /*if (soPozitivna && tabela[i] < 0){
             continue;
-        } /*else if ((tabela[i] + tabela[i + 1]) < tabela[i]){
+        } else if ((tabela[i] + tabela[i + 1]) < tabela[i]){
             continue;
-        } */else  { 
-            for (int j = i; j < n; j++){
-                vsota += tabela[j];
-                if (vsota > najVsota){
-                    najVsota = vsota;
-                }
-            }
+        } /else  {*/
+
+        if (vsota > najVsota){
+            najVsota = vsota;
+        }
+
+        if (vsota < 0){
+            vsota = 0;
         }
     }
     printf("%d\n", najVsota);
