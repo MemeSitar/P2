054ae877fc5600234fd3b79be97b9faa031572bc
MemeSitar <jurij.sitar@mjau.si>
2023-04-06 13:10:07 +0200

    evo dela

--- vaje05/naloga.c
+++ vaje05/naloga.c
@@ -22,7 +22,20 @@ void indeksInKazalec(int* t, int* indeks, int** kazalec) {
 }
 
 void frekvenceCrk(char* niz, int** frekvence) {
-    // dopolnite ...
+    int *t = malloc(26*sizeof(int));
+    for (int i = 0; i < 26; i++){
+        t[i] = 0;
+    }
+    
+    for(int i = 0; niz[i] != '\0'; i++){
+        if (niz[i] >= 'a' && niz[i] <= 'z'){
+            t[niz[i] - 'a']++;
+        }
+        if (niz[i] >= 'A' && niz[i] <= 'Z'){
+            t[niz[i] - 'A']++;
+        }
+    }
+    *frekvence = t;
 }
 
 #ifndef test
