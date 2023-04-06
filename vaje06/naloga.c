32e3acafd2874bb6d27c55494ee1c54b2ce838fd
MemeSitar <jurij.sitar@mjau.si>
2023-04-06 13:15:26 +0200

    dodal vaje 6

---
+++ vaje06/naloga.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "naloga.h"
+
+int steviloZnakov(char* niz, char znak) {
+    // popravite / dopolnite ...
+    return -1;
+}
+
+char* kopirajDoZnaka(char* niz, char znak) {
+    // popravite / dopolnite ...
+    return NULL;
+}
+
+char** razcleni(char* niz, char locilo, int* stOdsekov) {
+    // popravite / dopolnite ...
+    return NULL;
+}
+
+#ifndef test
+
+int main() {
+    // koda za ro"cno testiranje (po "zelji)
+
+    return 0;
+}
+
+#endif
