0f1f922b6bd71d65fd2e4dd86574d2805ac2474a
MemeSitar <jurij.sitar@mjau.si>
2023-04-18 20:07:29 +0200

    Začel nalogo
    Začel sem prvo nalogo prvega kolokvija.

---
+++ vaje-kolokviji/1kol18/1nal1/naloga1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+    // t[0] = -100000
+    // t[100000] = 0
+    // t[200000] = 100000
+    int* t = calloc(200001, sizeof(int));
+
+    
+}
