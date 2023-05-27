1411cff03bb18ddfefd3d7e800ff563099455b58
MemeSitar <jurij.sitar@mjau.si>
2023-05-27 13:53:33 +0200

    Dela za en testni primer!
    Osnovna funkcionalnost.

--- vaje09/nizi/nizi.c
+++ vaje09/nizi/nizi.c
@@ -4,34 +4,34 @@
 
 #define __
 
-void iteracija(int meja, char c1, char c2);
+void iteracija(int meja, int trenutna, char c1, char c2, char* crke);
 
 int main(){
+    int trenutna = 0;
     int zgMeja = 0;
     char c1, c2;
     scanf("%d %c %c", &zgMeja, &c1, &c2);
-
+    char* crke = calloc(zgMeja + 1, sizeof(char));
+    crke[zgMeja] = '\0';
     #ifdef __DEBUG__
         putchar(c1);
         putchar(c2);
         putchar('\n');
     #endif
 
-    iteracija(zgMeja, c1, c2);
+    iteracija(zgMeja, trenutna, c1, c2, crke);
     return 0;
 }
 
-void iteracija(int meja, char c1, char c2){
-    if (c1 == c2){
-        return;
-    }
-    if (meja == 0){
+void iteracija(int meja, int trenutna, char c1, char c2, char* crke){
+    if (c1 == c2 || meja == 0){
         return;
     }
     char tmpc1 = c1;
-    char tmpc2 = c2;
-    while(c1 <= c2){
-        putchar(c1);
-        
+    while(tmpc1 <= c2){
+        crke[trenutna] = tmpc1;
+        printf("%s\n", crke);
+        iteracija(meja - 1, trenutna + 1, c1, c2, crke);
+        tmpc1++;
     }
 }
