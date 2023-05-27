3a8f8bd46df13986c1fc4c3d7439f18b93daee33
MemeSitar <jurij.sitar@mjau.si>
2023-05-27 17:24:52 +0200

    Prva dva testa delata, ostalo je pa neumnost

--- vaje10/naloga.c
+++ vaje10/naloga.c
@@ -6,18 +6,38 @@
 #include "naloga.h"
 
 int poisciStudenta(Student** studentje, int stStudentov, int vpisna) {
-    // popravite / dopolnite ...
-    return 42;
+    for(int i = 0; i < stStudentov; i++){
+        if(studentje[i]->vpisna == vpisna){
+            return i;
+        }
+    }
+    return -1;
 }
 
 int poisciPO(Student* student, char* predmet) {
-    // popravite / dopolnite ...
-    return 42;
+    for(int i = 0; i < student->stPO; i++){
+        if (strcmp(student->po[i].predmet, predmet)==0 ){
+            return i;
+        }
+    }
+    return -1;
 }
 
 int dodaj(Student** studentje, int stStudentov, int vpisna, char* predmet, int ocena) {
-    // popravite / dopolnite ...
-    return 42;
+    int indeks = poisciStudenta(studentje, stStudentov, vpisna);
+    if (indeks == -1){
+        studentje[stStudentov] = (Student[]) {vpisna, (PO[]){{predmet, ocena}}, 1};
+        stStudentov++;
+    } else {
+        int indeksPo = poisciPO(studentje[indeks], predmet);
+        if(indeksPo == -1){
+            studentje[indeks]->po[studentje[indeks]->stPO] = (PO){predmet, ocena};
+            studentje[indeks]->stPO++;
+        } else {
+            studentje[indeks]->po[indeksPo].ocena = ocena;
+        }
+    }
+    return stStudentov;
 }
 
 #ifndef test
