31b0e7abafad715348229093664ab98cf005efe9
MemeSitar <jurij.sitar@mjau.si>
2023-06-03 16:31:47 +0200

    Dela za vse teste :)

--- vaje-kolokviji/2kol23b/naloga1/naloga1.c
+++ vaje-kolokviji/2kol23b/naloga1/naloga1.c
@@ -7,7 +7,24 @@
 // po potrebi dopolnite ...
 
 int main(int argc, char** argv) {
-    // dopolnite ...
+    FILE* vhodna = fopen(argv[1], "r");
+    FILE* izhodna = fopen(argv[2], "w");
 
+    bool zacetek = true;
+    char c;
+
+    while((c = fgetc(vhodna)) != EOF){
+        if (zacetek && c == ' '){
+            while((c = fgetc(vhodna)) == ' ');
+        }
+        fputc(c, izhodna);
+        zacetek = false;
+        if (c == '\n'){
+            zacetek = true;
+        }
+    }
+
+    fclose(vhodna);
+    fclose(izhodna);
     return 0;
 }
