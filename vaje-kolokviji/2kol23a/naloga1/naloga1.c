9cbbd11df1cabc4d047e44f1cefd854305da2a27
MemeSitar <jurij.sitar@mjau.si>
2023-06-03 13:08:26 +0200

    Naloga 1 dela za vse teste :)

--- vaje-kolokviji/2kol23a/naloga1/naloga1.c
+++ vaje-kolokviji/2kol23a/naloga1/naloga1.c
@@ -6,8 +6,37 @@
 
 // po potrebi dopolnite ...
 
-int main(int argc, char** argv) {
-    // dopolnite ...
+void delaj(FILE* vhodna, FILE* izhodna);
 
+int main(int argc, char** argv) {
+    char c;
+    FILE* vhodna = fopen(argv[1], "r");
+    FILE* izhodna = fopen(argv[2], "w");
+
+    while((c = fgetc(vhodna)) != EOF){
+        if (c == '"'){
+            delaj(vhodna, izhodna);
+        } else {
+            fputc(c, izhodna);
+        }
+    } 
+
+    fclose(vhodna);
+    fclose(izhodna);
     return 0;
+}
+
+void delaj(FILE* vhodna, FILE* izhodna){
+    char c = fgetc(vhodna);
+    if (c == 'S' || c == 's' || c == 'Z' || c == 'z' || c == 'C' || c == 'c'){
+        fputc(c, izhodna);
+        return;
+    }
+    if (c == '"'){
+        fputc(c, izhodna);
+        delaj(vhodna, izhodna);
+        return;
+    }
+    fputc('"', izhodna);
+    fputc(c, izhodna);
 }
