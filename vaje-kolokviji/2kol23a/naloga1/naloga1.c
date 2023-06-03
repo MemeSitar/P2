afab07e1224542abf3c2329195fc75e43e9a0079
MemeSitar <jurij.sitar@mjau.si>
2023-06-03 12:49:37 +0200

    Dodal še letošnja kolokvija!

---
+++ vaje-kolokviji/2kol23a/naloga1/naloga1.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// po potrebi dopolnite ...
+
+int main(int argc, char** argv) {
+    // dopolnite ...
+
+    return 0;
+}
