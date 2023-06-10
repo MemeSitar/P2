cacee00b9ae19235d81c178877d7ca35f7d6ff05
MemeSitar <jurij.sitar@mjau.si>
2023-06-10 23:09:59 +0200

    2. naloga dodana ampak še ne dela.

--- vaje-kolokviji/2kol23a/naloga2/naloga2.c
+++ vaje-kolokviji/2kol23a/naloga2/naloga2.c
@@ -17,13 +17,45 @@
 #include <string.h>
 
 #include "naloga2.h"
+void odstraniTrenutno(Vozlisce* prejsnje);
 
 // po potrebi dopolnite ...
 
 void izlociSkupne(Vozlisce* a, Vozlisce* b, Vozlisce** noviA, Vozlisce** noviB) {
-    // dopolnite ...
+    noviA = a;
+    noviB = b;
+    
+    Vozlisce* Atrenutno = a;
+    Vozlisce* Aprejsnje = a;
+    Vozlisce* Btrenutno = b;
+    Vozlisce* Bprejsnje = b;
+
+    int Avrednost = 0;
+    int Bvrednost = 0;
+
+    while(Atrenutno != NULL){
+        Avrednost = Atrenutno->podatek;
+        
+        
+    }
 }
 
+void odstraniTrenutno(Vozlisce* prejsnje){
+    Vozlisce* trenutno = prejsnje->naslednje;
+    prejsnje->naslednje = trenutno->naslednje;
+    free(trenutno);
+}
+
+void primerjaj(Vozlisce* vozlisceA, Vozlisce* vozlisceB){
+    int Avrednost = vozlisceA->podatek;
+    Vozlisce* Bprejsnje = vozlisceB;
+
+    while(vozlisceB != NULL){
+        if(vozlisceB->podatek == Avrednost){
+
+        }
+    }
+}
 //=============================================================================
 
 #ifndef test
