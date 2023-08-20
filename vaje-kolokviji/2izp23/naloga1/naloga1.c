c14454fe0479f6b3f9c87638e9392e173d7c94e8
MemeSitar <jurij.sitar@mjau.si>
2023-08-20 18:19:22 +0200

    prva naloga

--- vaje-kolokviji/2izp23/naloga1/naloga1.c
+++ vaje-kolokviji/2izp23/naloga1/naloga1.c
@@ -14,7 +14,21 @@
 // po potrebi dopolnite ...
 
 int main(int argc, char** argv) {
-    // dopolnite ...
+    FILE * vhodna = fopen(argv[1], "rb");
+    FILE * izhodna = fopen(argv[2], "wb");
 
+    __u_int buff = 0x0;
+    __u_int byte = fgetc(vhodna);
+
+
+    while((byte = fgetc(vhodna)) != EOF){
+        if (buff != byte)
+        {
+            fputc(byte, izhodna);
+            buff = byte;
+        }
+    }
+    fclose(izhodna);
+    fclose(vhodna);
     return 0;
 }
